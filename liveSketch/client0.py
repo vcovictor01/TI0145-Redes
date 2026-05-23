3e3705fb792f5b041f7c838437f70fb66558c73c
vcovictor01 <vcovictor01@gmail.com>
2026-05-23 20:35:49 -0300

    Teste de Sockets em loopback - rewrite

--- liveSketch/client0.py
+++ liveSketch/client0.py
@@ -5,18 +5,18 @@ SERVER_IP = 'localhost'
 PORT = 6666
 
 socketC0 = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #Socket IPv4 TCP
-print(f"[*] Tentando se conectar ao servidor {SERVER_IP}:{PORT}...")
+print(f"* Tentando se conectar ao servidor {SERVER_IP}:{PORT}")
 
 socketC0.connect((SERVER_IP, PORT)) #Conecta ao servidor
-print("[+] Conectado ao servidor!")
+print("+ Conectado ao servidor")
 
-msg = "Olá, Servidor! Sou o Cliente do SocketSketch."
+msg = "Olá, Servidor!"
 socketC0.sendall(msg.encode('utf-8')) #Envia mensagem para o servidor
-print("[->] Mensagem de teste enviada.")
+print("e Mensagem de teste enviada.")
 
 resposta = socketC0.recv(1024).decode('utf-8') #Agurdando resposta
-print(f"[<-] Resposta do servidor: '{resposta}'")
-print("[*] Encerrando socket do cliente.")
+print(f"r Resposta do servidor: '{resposta}'")
+print("* Encerrando socket do cliente.")
 
 #Encerra sockets
 socketC0.close()
--- liveSketch/server0.py
+++ liveSketch/server0.py
@@ -7,18 +7,18 @@ socketS0 = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #Socket IPv4 TCP
 socketS0.bind((HOST, PORT))
 
 socketS0.listen(1) #Escutando
-print(f"[*] Servidor liveSketch rodando na porta {PORT}.")
-print("[*] Aguardando conexão do cliente...")
+print(f"* Servidor rodando na porta {PORT}.")
+print("* Aguardando conexão do cliente...")
 
 socketC0, clientAddress = socketS0.accept() #Dados do cliente pelo socket
-print(f"[+] Conexão estabelecida com sucesso com: {clientAddress}")
+print(f"+ Conexão estabelecida com: {clientAddress}")
 
 msgA = socketC0.recv(1024).decode('utf-8') #Mensagem recebida do cliente
-print(f"[<-] Mensagem recebida do cliente: '{msgA}'")
+print(f"r Mensagem do cliente: '{msgA}'")
 
 msgB = "Conexão confirmada!"
 socketC0.sendall(msgB.encode('utf-8')) #Envia mensagem pro cliente
-print("[->] Resposta de confirmação enviada. Fechando conexões.")
+print("e Confirmação enviada. Fechando conexões.")
 
 #Encerra sockets
 socketS0.close()
